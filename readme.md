3f35ef764f520a63dea91428baa1f6b4a448f3d8
adabru <b.brunnmeier@gmx.de>
2020-01-29 07:59:30 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,20 @@
+
+# Educational Webhosting
+
+Nowadays in Europe you get a powerful webserver for 3€ per month ([e.g. from scaleway](https://www.scaleway.com/en/virtual-instances/arm-instances/)). You could also get half a year of free google server, but why bother switching after the free trial period if you can get something for just 3€. With proper configuration the server includes:
+
+- unlimited emails
+- webhosting
+- cloud storage
+
+Maybe every citizen should be sponsored with a free web server? ;-)
+
+This repository contains a nodejs server-script that enables easy-to-use static hosting, mainly intended for usage by pupils in an educational context. Without registration, without emails, without advertising.
+
+## Setup
+
+```
+git clone https://github.com/adabru/educational-webhosting.git
+cd educational-webhosting
+node server.js
+```
---
+++ server.js
@@ -0,0 +1,90 @@
+const http = require('http')
+const fs = require('fs').promises
+fs.createReadStream = require('fs').createReadStream
+const path = require('path')
+const url =  require('url')
+
+let port = 2222
+let host = '::1'
+let token = process.argv[2] || process.env.TOKEN
+if (!token) {
+  console.log('\nmissing TOKEN argument: either as first command line argument or as TOKEN environment variable\n')
+  return
+}
+
+let server = http.createServer( async (req, res) => {
+  try {
+    let _url = url.parse(req.url, true)
+
+    if(_url.pathname.endsWith('/'))
+      _url.pathname += 'index.html'
+
+    if (_url.pathname != path.normalize(_url.pathname)) {
+      res.writeHead(400)
+      res.end('invalid path')
+    } else if (_url.pathname == '/upload' && req.method == 'POST') {
+      let body = []
+      let size = 0
+      req.on('data', (data) => {
+        body.push(data)
+        size += data.length
+        if (size > 100e6) {
+          res.writeHead(413)
+          res.end('max 1 MB', null, () => req.destroy())
+        }
+      })
+      req.on('error', (e) => {
+        console.error(e)
+        res.writeHead(500)
+        res.end('error')
+      })
+      req.on('end', async () => {
+        let all = Buffer.concat(body)
+        console.log(all.toString('utf-8', 0, 600))
+        res.writeHead(200)
+        res.end('ok')
+      })
+    } else {
+      fs.createReadStream(`./public${_url.pathname}`)
+      .on('error', () => {
+        res.writeHead(404)
+        res.end('not found') })
+      .on('open', () => {
+        contenttypes = {
+          '.js': 'application/javascript',
+          '.css': 'text/css',
+          '.styl': 'text/css',
+          '.svg': 'image/svg+xml',
+          '.png': 'image/png',
+          '.jpg': 'image/jpg',
+          '.pdf': 'application/pdf',
+          '.html': 'text/html; charset=utf-8',
+          '.htm': 'text/html; charset=utf-8'
+        };
+        // 'Content-Type': 'text/html; charset=utf-8'
+        res.writeHead(200, {'Content-Type': contenttypes[path.extname(_url.pathname)] || 'text/plain; charset=utf-8'})})
+      .pipe(res)
+    }
+  }
+  catch (e) {
+    console.error(e)
+    res.writeHead(500)
+    res.end('server error')
+  }
+})
+
+server.listen(port, host, () => console.log(`http-server running at http://${host}:${port}/`))
+
+server.on('error', e => {
+  console.log('An error occured while serving: ' + e)
+  setTimeout(() => {
+      server.close();
+      server.listen(port, host);
+    }, 1000)
+})
+
+process.on('SIGINT', function () {
+  try { server.close() }
+  catch(e) { }
+  process.exit()
+})
